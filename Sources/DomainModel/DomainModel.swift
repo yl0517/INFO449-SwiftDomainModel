15ffdb635cf0573319c4b2fb1f3a4ea445048e09
yl0517 <yl0517@uw.edu>
2025-04-14 17:38:36 -0700

    finished money

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,26 +9,59 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount: Int
+    var currency: String
+    private let validCurrencies = ["USD", "EUR", "GBP", "CAN"]
+    private let conversionRates: [String: Double] = ["USD": 1.0, "EUR": 1.5, "GBP": 0.5, "CAN": 1.25]
+    
+    init(amount: Int, currency: String) {
+            self.amount = amount
+            if !validCurrencies.contains(currency) {
+                self.currency = ""
+            } else {
+                self.currency = currency
+            }
+        }
+    
+    func convert(_ currency : String) -> Money {
+        if !validCurrencies.contains(currency) {
+            fatalError("Invalid currency")
+        }
+        
+        var converted = (Double(self.amount)/conversionRates[self.currency]!) * conversionRates[currency]!
+        converted.round()
+        return Money(amount: Int(converted), currency: currency)
+    }
+    
+    func add(_ amount: Money) -> Money {
+        let convertedAmt = amount.convert(self.currency)
+        return Money(amount: self.amount + convertedAmt.amount, currency: self.currency)
+    }
+    
+    func subtract(_ amount: Money) -> Money {
+        let convertedAmt = amount.convert(self.currency)
+        return Money(amount: self.amount - convertedAmt.amount, currency: self.currency)
+    }
 }
 
 ////////////////////////////////////
 // Job
 //
-public class Job {
-    public enum JobType {
-        case Hourly(Double)
-        case Salary(UInt)
-    }
-}
-
-////////////////////////////////////
-// Person
+//public class Job {
+//    public enum JobType {
+//        case Hourly(Double)
+//        case Salary(UInt)
+//    }
+//}
 //
-public class Person {
-}
-
-////////////////////////////////////
-// Family
+//////////////////////////////////////
+//// Person
+////
+//public class Person {
+//}
 //
-public class Family {
-}
+//////////////////////////////////////
+//// Family
+////
+//public class Family {
+//}
